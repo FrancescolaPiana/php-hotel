ea7f6175313b57fda9d9ffbb61c1e2477764abbb
Francescolapiana <Lushika1219@gmail.com>
2022-11-29 18:00:21 +0100

    completo

--- index.php
+++ index.php
@@ -1,81 +1,82 @@
 <?php
-$comics = [
-    [
-        "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
-        "price" => "$19.99",
-        "series" => "Action Comics",
-        "type" => "comic book"
-    ],
-    [
-        "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
-        "price" => "$3.99",
-        "series" => "American Vampire 1976",
-        "type" => "comic book"
-    ],
-    [
-        "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
-        "price" => "$16.99",
-        "series" => "Aquaman",
-        "type" => "graphic novel"
-    ],
-    [
-        "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
-        "price" => "$2.99",
-        "series" => "Batgirl",
-        "type" => "comic book"
-    ],
-    [
-        "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
-        "price" => "$3.99",
-        "series" => "Batman",
-        "type" => "comic book"
-    ],
-    [
-        "thumb" => "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1MAXI008ISBN_0.jpg",
-        "price" => "$2.99",
-        "series" => "Batman Beyond",
-        "type" => "comic book"
-    ],
-    [
-        "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
-        "price" => "$3.99",
-        "series" => "Batman/Superman",
-        "type" => "comic book"
-    ],
-    [
-        "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
-        "price" => "$4.99",
-        "series" => "Batman/Superman Annual",
-        "type" => "comic book"
-    ],
-    [
-        "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
-        "price" => "$5.99",
-        "series" => "Batman=> The Joker War Zone",
-        "type" => "comic book"
-    ],
-    [
-        "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
-        "price" => "$6.99",
-        "series" => "Batman=> Three Jokers",
-        "type" => "comic book"
-    ],
-    [
-        "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
-        "price" => "$4.99",
-        "series" => "Batman=> White Knight Presents=> Harley Quinn",
-        "type" => "comic book"
-    ],
-    [
-        "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
-        "price" => "$16.99",
-        "series" => "Catwoman",
-        "type" => "graphic novel"
-    ]
-];
+// $comics = [
+//     [
+//         "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+//         "price" => "$19.99",
+//         "series" => "Action Comics",
+//         "type" => "comic book"
+//     ],
+//     [
+//         "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+//         "price" => "$3.99",
+//         "series" => "American Vampire 1976",
+//         "type" => "comic book"
+//     ],
+//     [
+//         "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+//         "price" => "$16.99",
+//         "series" => "Aquaman",
+//         "type" => "graphic novel"
+//     ],
+//     [
+//         "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+//         "price" => "$2.99",
+//         "series" => "Batgirl",
+//         "type" => "comic book"
+//     ],
+//     [
+//         "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+//         "price" => "$3.99",
+//         "series" => "Batman",
+//         "type" => "comic book"
+//     ],
+//     [
+//         "thumb" => "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1MAXI008ISBN_0.jpg",
+//         "price" => "$2.99",
+//         "series" => "Batman Beyond",
+//         "type" => "comic book"
+//     ],
+//     [
+//         "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+//         "price" => "$3.99",
+//         "series" => "Batman/Superman",
+//         "type" => "comic book"
+//     ],
+//     [
+//         "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+//         "price" => "$4.99",
+//         "series" => "Batman/Superman Annual",
+//         "type" => "comic book"
+//     ],
+//     [
+//         "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+//         "price" => "$5.99",
+//         "series" => "Batman=> The Joker War Zone",
+//         "type" => "comic book"
+//     ],
+//     [
+//         "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+//         "price" => "$6.99",
+//         "series" => "Batman=> Three Jokers",
+//         "type" => "comic book"
+//     ],
+//     [
+//         "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+//         "price" => "$4.99",
+//         "series" => "Batman=> White Knight Presents=> Harley Quinn",
+//         "type" => "comic book"
+//     ],
+//     [
+//         "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+//         "price" => "$16.99",
+//         "series" => "Catwoman",
+//         "type" => "graphic novel"
+//     ]
+// ];
 
 $hotels = [
     [
+        'url' => 'https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         'name' => 'Hotel Belvedere',
         'description' => 'Hotel Belvedere Descrizione',
         'parking' => true,
@@ -83,6 +84,7 @@ $hotels = [
         'distance_to_center' => 10.4
     ],
     [
+        'url' => 'https://images.pexels.com/photos/1714975/pexels-photo-1714975.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         'name' => 'Hotel Futuro',
         'description' => 'Hotel Futuro Descrizione',
         'parking' => true,
@@ -90,6 +92,7 @@ $hotels = [
         'distance_to_center' => 2
     ],
     [
+        'url' => 'https://images.pexels.com/photos/787871/pexels-photo-787871.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         'name' => 'Hotel Rivamare',
         'description' => 'Hotel Rivamare Descrizione',
         'parking' => false,
@@ -97,6 +100,7 @@ $hotels = [
         'distance_to_center' => 1
     ],
     [
+        'url' => 'https://images.pexels.com/photos/297983/pexels-photo-297983.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         'name' => 'Hotel Bellavista',
         'description' => 'Hotel Bellavista Descrizione',
         'parking' => false,
@@ -104,6 +108,7 @@ $hotels = [
         'distance_to_center' => 5.5
     ],
     [
+        'url' => 'https://images.pexels.com/photos/12284844/pexels-photo-12284844.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         'name' => 'Hotel Milano',
         'description' => 'Hotel Milano Descrizione',
         'parking' => true,
@@ -128,16 +133,31 @@ $hotels = [
 // }
 
 if (isset($_GET['type']) && !empty($_GET['type'])) {
-    $tempcomics = [];
-    foreach ($comics as $item) {
-        if ($item['type'] == $_GET['type']) {
-            $tempcomics[] = $item;
+    $temparr = [];
+    foreach ($hotels as $item) {
+        if ($item['parking'] == true) {
+            $temparr[] = $item;
         }
     }
     ;
-    $comics = $tempcomics;
+    $hotels = $temparr;
     // var_dump($comics);
 }
+;
+
+if (isset($_GET['vote']) && !empty($_GET['vote'])) {
+    $temparr = [];
+    foreach ($hotels as $item) {
+        if ($item['vote'] == $_GET['vote']) {
+            $temparr[] = $item;
+        }
+    }
+    ;
+    $hotels = $temparr;
+    var_dump($_GET);
+}
+;
+
 
 ?>
 <!DOCTYPE html>
@@ -147,6 +167,15 @@ if (isset($_GET['type']) && !empty($_GET['type'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- bootstrap CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
     <title>Document</title>
 </head>
 
@@ -154,9 +183,16 @@ if (isset($_GET['type']) && !empty($_GET['type'])) {
     <header>
         <form action="index.php" method="GET" name="formFilter">
             <select name="type" id="type">
-                <option value="">Select options</option>
+                <option value="">No filter</option>
                 <option value="parking">Parking</option>
-                <option value="stars">Stars</option>
+            </select>
+            <select name="vote" id="vote">
+                <option value="">No filter</option>
+                <option value="1">Una stella</option>
+                <option value="2">Due stelle</option>
+                <option value="3">Tre stelle</option>
+                <option value="4">Quattro stelle</option>
+                <option value="5">Cinque stelle</option>
             </select>
             <button type="submit">Filtra</button>
         </form>
@@ -167,7 +203,7 @@ if (isset($_GET['type']) && !empty($_GET['type'])) {
         ?>
 
         <div class="card">
-            <img src="" alt="">
+            <img class="img-fluid" src="<?php echo $item['url'] ?>" alt="">
             <h3>
                 <?php echo $item['name'] ?>
             </h3>
@@ -176,9 +212,16 @@ if (isset($_GET['type']) && !empty($_GET['type'])) {
             </h4>
             <h6>
                 <?php echo $item['vote'] ?>
+                <i class="fa-solid fa-star"></i>
             </h6>
             <h6>
-                <?php echo $item['parking'] ?>
+                <?php
+            if ($item['parking']) {
+                echo 'Hotel Con Parcheggio';
+            } else {
+                echo 'NO Parcheggio';
+            }
+                ?>
             </h6>
         </div>
 
@@ -201,9 +244,17 @@ if (isset($_GET['type']) && !empty($_GET['type'])) {
         gap: 42px;
         flex-wrap: wrap;
         width: 1170px;
-        background-color: rgba(20, 20, 20, .1);
+        /* background-color: rgba(20, 20, 20, .1); */
         margin: 2rem auto;
-        padding: 40px;
+        padding: 0px;
+    }
+
+    h3 {
+        font-size: 1.5rem;
+    }
+
+    h4 {
+        font-size: 1.2rem;
     }
 
     .card {
